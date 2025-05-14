535ec1eaa81f6726d4a66a8654c881970e73d535
edukrause <eduardokrause678@gmail.com>
2025-05-14 19:20:54 -0300

    traia

---
+++ aula_21/src/main/java/org/example/Atividade_3.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Atividade_3 {
+    public static void main(String[] args) {
+
+        char[] nome = {'J', 'A', 'V', 'A'};
+        for (int i = 0; i < 3; i++) {
+            System.out.println(nome[i]);
+        }
+        System.out.println("\n"+nome[2]);
+    }
+}
---
+++ aula_21/src/main/java/org/example/Atividade_4.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Atividade_4 {
+    public static void main(String[] args) {
+
+        double[] valores = {1, 2, 3, 4, 5, 6, 7};
+        for (int i = 0; i < 7; i++) {
+            System.out.println(" "+valores[i]);
+        }
+    }
+}
